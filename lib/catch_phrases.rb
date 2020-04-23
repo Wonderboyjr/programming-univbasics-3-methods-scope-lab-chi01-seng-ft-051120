c8708065fd1333399db5217a0ed55c3c58088377
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-23 01:40:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,21 @@
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  $phrase = "It's-a me, Mario!"
+  puts $phrase
 end
 
 def toadstool
-  puts status
+  $status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts $status
+end
+
+def link
+  $sword = "It's Dangerous To Go Alone! Take This."
+  puts $sword
+end
+  
+def all_phrases
+  puts $phrase
+  puts $status
+  puts $sword
 end
